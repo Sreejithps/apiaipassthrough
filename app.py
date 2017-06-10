be534bf20b1d9a4db9c9cedf2738e269ff003ac7
Sreejith Sreedharan <sreejith.ps@hotmail.com>
2017-06-10 23:35:44 +0530

    Req .txt

--- app.py
+++ app.py
@@ -1,26 +1,19 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
-from future.standard_library import install_aliases
-install_aliases()
 
-from urllib.parse import urlparse, urlencode
-from urllib.request import urlopen, Request
-from urllib.error import HTTPError
-
-import json
 import os
 
 from flask import Flask
 from flask import request
 from flask import make_response
-
 import requests
 
 # Flask app should start in global layout
 app = Flask(__name__)
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
+
+
 
 
 @app.route('/webhook', methods=['GET'])
