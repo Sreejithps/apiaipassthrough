34a6d1531a73e31556dd4220bf864fb3e170afd6
Sreejith Sreedharan <sreejith.ps@hotmail.com>
2017-06-10 23:26:37 +0530

    a

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+from future.standard_library import install_aliases
+install_aliases()
+
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
+import json
 import os
+
 from flask import Flask
+from flask import request
+from flask import make_response
+
 import requests
 
 # Flask app should start in global layout
@@ -10,9 +23,8 @@ app = Flask(__name__)
 
 @app.route('/webhook', methods=['GET'])
 def webhook():
-    # response = requests.get('http://www.google.com')
-    # return response.text
-    return 'H'
+    response = requests.get('http://www.google.com')
+    return response.text
 
 
 if __name__ == '__main__':
