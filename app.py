68834ed62ac3dc39f573ea93392c07b449a08e48
Sreejith Sreedharan <sreejith.ps@hotmail.com>
2017-06-10 21:11:23 +0530

    Test

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
+    return 'Hello world!'
     req = request.get_json(silent=True, force=True)
 
     print("Request:")
