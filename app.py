1c9a5926d3c8e30b198a6208fcfcde5e59c70109
Sreejith Sreedharan <sreejith.ps@hotmail.com>
2018-09-17 14:45:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,10 +21,7 @@ app.logger.setLevel(logging.ERROR)
 @app.route('/webhook', methods=['POST'])
 def webhook():
     if request.headers['apikey'] == 'a3be1e29-8d95-474c-9ae8-faa88ade48b4':
-        response = requests.get('http://noecommercews1098.cloudapp.net/api.ai/webhook.ashx', data = request.data)
-        return response.text
-    else:
-        return 'foo'
+        return 'Hello how are you?'
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
