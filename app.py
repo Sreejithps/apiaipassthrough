d87922f728201a075a50ccc2429067e2658e710f
Sreejith Sreedharan <sreejith.ps@hotmail.com>
2018-09-17 15:03:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ if __name__ == '__main__':
 
     print("Starting app on port %d" % port)
 
-    app.run(debug=False, port=5000, host='0.0.0.0')
+    app.run(debug=False, port=port, host='0.0.0.0')
